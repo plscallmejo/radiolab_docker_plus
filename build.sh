54351354ff94bff93060a804a241dac1f63c9c30
Haoming Huang <fsplscallmejo@live.com>
2021-02-26 10:03:33 +0800

    Build script to control building processes

--- build.sh
+++ build.sh
@@ -1,20 +1,38 @@
 #!/bin/bash
 
+# Set colors
+normal="\033[0m"
+error="\033[41;33m"
+warning="\033[43;31m"
+proceed="\033[42;30m"
+hint="\033[4;36m"
+ERROR="${error}ERROR${normal}"
+WARNING="${warning}WARNING${normal}"
+PROCEED="${proceed}PROCEEDING${normal}"
+
 # Get runtime option
-while getopts ":r" opt
+while getopts "r:" opt
 do
     case ${opt} in
     r)
         RUNTIME=${OPTARG}
         ;;
+    \?)
+        echo -e "${ERROR}: Invalid option."
+        exit 1
+        ;;
     esac
 done
 
-# Build a basic docker image
+# Setting a basic docker image
 mkdir -p build/base
 touch build/base/Dockerfile
-RUNTIME="intel"
-if [[ $RUNTIME = "nvidia" ]]; then
+if [[ ! -n ${RUNTIME} ]]; then
+    echo -e "${WARNING}: no ${hint}-r${normal} (RUNTIME) option was supplied, so automatically setting to \"${hint}intel${normal}\""
+    RUNTIME="intel"
+fi
+if [[ ${RUNTIME} = "nvidia" ]]; then
+echo -e "${PROCEED}: Building base image from \"${hint}nvidia/cudagl:9.1-runtime-ubuntu16.04${normal}\ with \"${hint}nvidia runtime${normal}\" support"
 echo '# nvidia/cudagl:9.1-runtime-ubuntu16.04
 FROM nvidia/cudagl:9.1-runtime-ubuntu16.04
 # nvidia-container-runtime
@@ -24,7 +42,8 @@ ENV NVIDIA_VISIBLE_DEVICES ${NVIDIA_VISIBLE_DEVICES:-all}
 ENV NVIDIA_DRIVER_CAPABILITIES ${NVIDIA_DRIVER_CAPABILITIES:+$NVIDIA_DRIVER_CAPABILITIES,}graphics
 RUN sed -i "s/archive.ubuntu.com/mirrors.tuna.tsinghua.edu.cn/g" /etc/apt/sources.list \
     && echo "deb https://mirrors.aliyun.com/nvidia-cuda/ubuntu1604/x86_64/ ./" > /etc/apt/sources.list.d/cuda.list'  > build/base/Dockerfile
-elif [[ $RUNTIME = "intel" ]]; then
+elif [[ ${RUNTIME} = "intel" ]]; then
+echo -e "${PROCEED}: Building base image from \"${hint}ubuntu:16.04${normal}\""
 echo '#ubuntu:16.04
 FROM ubuntu:16.04
 # mesa runtime
@@ -36,10 +55,16 @@ RUN apt-get update -qq \
            qt5-default \
     && apt-get clean \
     && rm -rf /var/lib/apt/lists/*' > build/base/Dockerfile
+else
+echo -e "${ERROR}: ${hint}-r${normal} (RUNTIME) option can only be either \"${hint}intel${normal}\" or \"${hint}nvidia${normal}\""
+exit 1
 fi
 
+## Build base image
 docker build -t radiolab_base:latest build/base
+echo -e "${PROCEED}: Base image build complete"
 
 # Build Docker image with proper runtime
+echo -e "${PROCEED}: Build \"${hint}radiolab${normal}\" image from base"
 docker build -t radiolab:latest build --build-arg RUNTIME=$RUNTIME
 
