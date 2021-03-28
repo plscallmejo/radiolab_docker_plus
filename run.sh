d31f58c0bbffc9d0a15947ea22ac274356f0448e
Haoming Hunag <fsplscallmejo@live.com>
2021-03-29 00:01:26 +0800

    improve run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
 
-docker exec -it radiolab_docker bash
+# Set colors
+normal="\033[0m"
+error="\033[41;33m"
+warning="\033[43;31m"
+proceed="\033[42;30m"
+hint="\033[4;36m"
+ERROR="${error}ERROR${normal}"
+WARNING="${warning}WARNING${normal}"
+PROCEED="${proceed}PROCEEDING${normal}"
+
+EXIST_DOCKER=`docker ps -a | grep radiolab_docker | awk '{print $NF}'`
+if [[ -z {EXIST_DOCKER} ]]; then
+    echo -e "${ERROR}: \"${hint}radiolab_docker${normal}\" dose not exist. Please run ${hint}create.sh${normal} first."
+    exit 1
+else
+    RUNNING_DOCKER=`docker ps -a | grep radiolab_docker | awk -F '   ' '{print $5}' | grep Up`
+    if [[ ! -z {RUNNING_DOCKER} ]]; then
+        echo -e "${PROCEED}: \"${hint}radiolab_docker${normal}\" is running."
+    else
+        echo -e "${PROCEED}: \"${hint}radiolab_docker${normal}\" is not running, bringing the container up online."
+        docker container start radiolab_docker > /dev/null 2>&1
+    fi
+    echo -e "${PROCEED}: Entering interactive shell."
+    docker exec -it radiolab_docker bash
+fi
+
+
+
