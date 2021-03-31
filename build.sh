b9d872342ffff9a65b9ba421419224fc7cbf6fc3
Haoming Huang <fsplscallmejo@live.com>
2021-04-01 01:19:44 +0800

    improve some lines

--- build.sh
+++ build.sh
@@ -10,15 +10,42 @@ ERROR="${error}ERROR${normal}"
 WARNING="${warning}WARNING${normal}"
 PROCEED="${proceed}PROCEEDING${normal}"
 
+# Usage
+Usage () {
+echo ""
+echo "Build the basic Dockerfile and docker-docker-compose.yml,"
+echo "then build the Docker image from the begining."
+echo ""
+echo "Usage: ./build.sh [options]"
+echo "-r   Specify the RUNTIME, either "normal" or "nvidia" (default: normal)"
+echo "-c   Flag to make the docker-compose.yml file only, no argument is needed."
+echo "     No Docker image will be built."
+echo "     For making or upating docker-compose.yml."
+echo "     e.g. you have pulled the image from somewhere else,"
+echo "          or you already have the image built, and don't want to re-build the image."
+echo ""
+echo "Examples:"
+echo "./build.sh -r nvidia       will build the Docker image using nvidia RUNTIME."
+echo "./build.sh -r nvidia -c    will make the docker-compose.yml file with nvidia RUNTIME only,"
+echo "                           but won't build the Docker image."
+echo "./build.sh -r normal       will build the Docker image with normal RUNTIME."
+echo "./build.sh -c              will make the docker-compose.yml file with normal RUNTIME only,"
+echo "                           but won't build the Docker image."
+}
+
 # Get runtime option
-while getopts "r:" opt
+while getopts "r:c" opt
 do
     case ${opt} in
     r)
         RUNTIME=${OPTARG}
         ;;
+    c)
+        COMPOSE="Y"
+        ;;
     \?)
         echo -e "${ERROR}: Invalid option."
+        Usage
         exit 1
         ;;
     esac
@@ -31,8 +58,9 @@ if [[ ! -n ${RUNTIME} ]]; then
     echo -e "${WARNING}: no ${hint}-r${normal} (RUNTIME) option was supplied, so automatically setting to \"${hint}normal${normal}\" Runtime."
     RUNTIME="normal"
 fi
-echo -e "${PROCEED}: Generating base ${hint}Dockerfile${normal}"
 if [[ ${RUNTIME} = "nvidia" ]]; then
+if [[ -z ${COMPOSE} ]]; then
+echo -e "${PROCEED}: Generating base ${hint}Dockerfile${normal}"
 echo -e "${PROCEED}: Building base image from \"${hint}nvidia/cudagl:9.1-runtime-ubuntu16.04${normal} with \"${hint}nvidia runtime${normal}\" support"
 echo '# nvidia/cudagl:9.1-runtime-ubuntu16.04
 FROM nvidia/cudagl:9.1-runtime-ubuntu16.04
@@ -43,6 +71,7 @@ ENV NVIDIA_VISIBLE_DEVICES ${NVIDIA_VISIBLE_DEVICES:-all}
 ENV NVIDIA_DRIVER_CAPABILITIES ${NVIDIA_DRIVER_CAPABILITIES:+$NVIDIA_DRIVER_CAPABILITIES,}graphics
 RUN sed -i "s/archive.ubuntu.com/mirrors.tuna.tsinghua.edu.cn/g" /etc/apt/sources.list \
     && echo "deb https://mirrors.aliyun.com/nvidia-cuda/ubuntu1604/x86_64/ ./" > /etc/apt/sources.list.d/cuda.list'  > build/base/Dockerfile
+fi
 echo -e "${PROCEED}: Generating ${hint}docker-compose.yml${normal}"
 echo '# docker-compose.yml that uses nvidia runtime
 version: "2.3"
@@ -68,6 +97,8 @@ services:
             - /etc/shadow:/etc/shadow:ro
         tty: true' > docker-compose.yml
 elif [[ ${RUNTIME} = "normal" ]]; then
+if [[ -z ${COMPOSE} ]]; then
+echo -e "${PROCEED}: Generating base ${hint}Dockerfile${normal}"
 echo -e "${PROCEED}: Building base image from \"${hint}ubuntu:16.04${normal}\""
 echo '#ubuntu:16.04
 FROM ubuntu:16.04
@@ -80,6 +111,7 @@ RUN apt-get update -qq \
            qt5-default \
     && apt-get clean \
     && rm -rf /var/lib/apt/lists/*' > build/base/Dockerfile
+fi
 echo -e "${PROCEED}: Generating ${hint}docker-compose.yml${normal}"
 echo '# docker-compose.yml that use normal
 version: "2.3"
@@ -104,9 +136,11 @@ services:
         tty: true' > docker-compose.yml
 else
 echo -e "${ERROR}: ${hint}-r${normal} (RUNTIME) option can only be either \"${hint}normal${normal}\" or \"${hint}nvidia${normal}\""
+Usage
 exit 1
 fi
 
+if [[ -z ${COMPOSE} ]]; then
 ## Build base image
 docker build -t radiolab_base:latest build/base
 echo -e "${PROCEED}: Base image build complete"
@@ -114,4 +148,4 @@ echo -e "${PROCEED}: Base image build complete"
 # Build Docker image with proper runtime
 echo -e "${PROCEED}: Build \"${hint}radiolab${normal}\" image from base"
 docker build -t radiolab:latest build --build-arg RUNTIME=$RUNTIME
-
+fi
--- create.sh
+++ create.sh
@@ -10,56 +10,74 @@ ERROR="${error}ERROR${normal}"
 WARNING="${warning}WARNING${normal}"
 PROCEED="${proceed}PROCEEDING${normal}"
 
-usage () {
-echo -e " \
-******************************** \n \
-*** Create a radiolab_docker *** \n \
-******************************** \n \
-Usage: create_container.sh [data_path] \n \
- [data_path] will be mounted to /DATA in inside the container."
-
+Usage () {
+echo ""
+echo "Create a radiolab_docker."
+echo ""
+echo "Usage: ./create.sh [data_path]"
+echo ""
+echo "Specify the "data_path" to be mounted to /DATA in inside the container."
+echo "Note, the "data_path" should be a DIR,"
+echo "      and you are supposed to own the rwx permissions to it."
+echo ""
+echo "EXAMPLES:"
+echo "./create.sh /the/path/to/your/data"
+echo ""
 }
 
 # Set Data_path
 if [[ -z $1 ]]; then
     echo -e "${ERROR}: Please specify the data path!"
-    usage
+    Usage
     exit 1
 else
     DATA_PATH=`readlink -e $1`
     if [[ -z ${DATA_PATH} ]]; then
-        echo -e "${ERROR}: The data path \"${hint}$1${normal}\" is not valid! Please check again!"
+        echo -e "${ERROR}: The data path \"${hint}$1${normal}\" is invalid! Please check again!"
+        Usage
         exit 1
     else
-        EXIST_DOCKER=`docker ps -a | grep radiolab_docker | awk '{print $NF}'`
-        if [[ ! -z ${EXIST_DOCKER} ]]; then
-            RUNNING_DOCKER=`docker ps -a | grep radiolab_docker | awk -F '   ' '{print $5}' | grep Up`
-            if [[ ! -z ${RUNNING_DOCKER} ]]; then
-                echo -e "${WARNING}: We found existing \"${hint}radiolab_docker${normal},\" and it's ${hint}RUNNING${normal}!"
-                echo -e "${WARNING}: This process will intent to ${hint}STOP ALL THE RUNNING PROCESSES${normal} in the current \"${hint}radiolab_docker${normal}\" instence and ${hint}RE-CREATE${normal} it."
+        if [[ -d ${DATA_PATH} ]]; then
+            if [[ -r ${DATA_PATH} && -r ${DATA_PATH} && -w {DATA_PATH} ]]; then
+                EXIST_DOCKER=`docker ps -a | grep radiolab_docker | awk '{print $NF}'`
+                if [[ ! -z ${EXIST_DOCKER} ]]; then
+                    RUNNING_DOCKER=`docker ps -a | grep radiolab_docker | awk -F '   ' '{print $5}' | grep Up`
+                    if [[ ! -z ${RUNNING_DOCKER} ]]; then
+                        echo -e "${WARNING}: We found existing \"${hint}radiolab_docker${normal},\" and it's ${hint}RUNNING${normal}!"
+                        echo -e "${WARNING}: This process intent to ${hint}STOP ALL THE RUNNING PROCESSES${normal} in the current \"${hint}radiolab_docker${normal}\" instence and ${hint}RE-CREATE${normal} it."
+                    else
+                        echo -e "${WARNING}: We found existing \"${hint}radiolab_docker${normal}.\""
+                        echo -e "${WARNING}: This process intent to ${hint}RE-CREATE${normal} it."
+                    fi
+                    echo -e "${WARNING}: Also note, the \"${hint}/DATA${normal}\" (container) will redirect to \"${hint}${DATA_PATH}${normal}\" (host). "
+                    read -r -p "Comfirm? [Y/N] " input
+                    case $input in
+                        [yY][eE][sS]|[yY])
+                            SEL="Y"
+                            ;;
+                        [nN][oO]|[nN])
+                            SEL="N"
+                            ;;
+                        *)
+                            echo "Invalid input..."
+                            exit 1
+                            ;;
+                    esac
+                    if [[ ${SEL} == "N" ]]; then
+                        exit 1
+                    fi
+                fi
+                echo -e "${PROCEED}: Creating radiolab docker"
+                CURRENT_UID=`id -u`:`id -g` DATA=${DATA_PATH} docker-compose up -d --force-recreate
             else
-                echo -e "${WARNING}: We found existing \"${hint}radiolab_docker${normal}.\""
-                echo -e "${WARNING}: This process will intent to ${hint}RE-CREATE${normal} it."
-            fi
-            echo -e "${WARNING}: Also note, the \"${hint}/DATA${normal}\" (container) will redirect to \"${hint}${DATA_PATH}${normal}\" (host). "
-            read -r -p "Comfirm? [Y/N] " input
-            case $input in
-                [yY][eE][sS]|[yY])
-                    SEL="Y"
-                    ;;
-                [nN][oO]|[nN])
-                    SEL="N"
-                    ;;
-                *)
-                    echo "Invalid input..."
-                    exit 1
-                    ;;
-            esac
-            if [[ ${SEL} == "N" ]]; then
+                echo -e "${ERROR}: your should own the rwx permissions to the data path \"${hint}$1${normal}\"! Please check again!"
+                Usage
                 exit 1
             fi
+        else
+            echo -e "${ERROR}: The data path \"${hint}$1${normal}\" should be a DIR or a file! Please check again!"
+            Usage
+            exit 1
         fi
-        echo -e "${PROCEED}: Creating radiolab docker"
-        CURRENT_UID=`id -u`:`id -g` DATA=${DATA_PATH} docker-compose up -d --force-recreate
     fi
 fi
