87feb8a598e04f2134c60d2ac627e3613562e019
Haoming Hunag <fsplscallmejo@live.com>
2021-04-10 17:56:15 +0800

    Add ss-libev control

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ echo "Build the basic Dockerfile and docker-docker-compose.yml,"
 echo "then build the Docker image from the begining."
 echo ""
 echo "Usage: ./build.sh [options]"
-echo "-r, --runtime        Specify the RUNTIME, either "normal" or "nvidia" (default: normal)"
+echo "-r, --runtime        Specify the RUNTIME, either "normal" or "nvidia". (default: normal)"
 echo "-c, --make-compose   Flag to make the docker-compose.yml file only, no argument is needed."
 echo "                     No Docker image will be built."
 echo "                     For making or upating docker-compose.yml."
@@ -26,6 +26,11 @@ echo "                         or you already have the image built, and don't wa
 echo "                     Note, the pre-built image should tag with "radiolab_docker:latest"."
 echo "                       You can use the following command:"
 echo "                         "docker tag \<pre-built image\> radiolab_docker:latest""
+echo "-s, --ss-libev       Use ss-libev through proxychains-ng."
+echo "                     T for TRUE, and F for FALSE. (default: T)"
+echo "                     Note, ss config file should be copied to the build/content folder and"
+echo "                       rename to shadowsocks.json"
+echo "                     Also Note, only simple-obfs plugin is supported"
 echo "-h, --help           Show this message."
 echo ""
 echo "Examples:"
@@ -39,18 +44,48 @@ echo "                           but won't build the Docker image."
 echo ""
 }
 
+# CN_procy_control
+cn_proxy() {
+    file=$1
+    active=$2
+
+    if [[ -z ${active} ]]; then
+        active=0
+    fi
+
+    line_edit=( $( \
+        awk -v line_begin="`sed -n -e "/^# CN_SP+/=" ${file} | sed -e "s/\(.\+\)/\1 + 1/g" | bc`" \
+            -v line_end="`sed -n -e "/^# CN_SP-/=" ${file} | sed -e "s/\(.\+\)/\1 - 1/g" | bc`" \
+            'BEGIN {
+                split(line_begin, begin, " ");
+                split(line_end, end, " ");
+                for (i in end) {print begin[i]","end[i]}}' ))
+
+    for lines in ${line_edit[@]};do
+        sed -i "${lines}{s/^\#\+//g}" ${file}
+    done
+
+        if [[ ${active} -eq 0 ]]; then
+            for lines in ${line_edit[@]};do
+                sed -i "${lines}{s/^\(.\+\)/#\1/g}" ${file}
+            done
+        fi
+}
+
+# Read arguments
 for arg in "$@"; do
   shift
   case "$arg" in
     "--runtime")      set -- "$@" "-p" ;;
     "--make-compose") set -- "$@" "-c" ;;
+    "--ss-libev")     set -- "$@" "-s" ;;
     "--help")         set -- "$@" "-h" ;;
     *)                set -- "$@" "$arg"
   esac
 done
 
 # Get runtime option
-while getopts "r:ch" opt
+while getopts "r:s:ch" opt
 do
     case ${opt} in
     r)
@@ -58,6 +93,9 @@ do
         ;;
     c)
         COMPOSE="Y"
+        ;;
+    s)
+        CNOPT=${OPTARG}
         ;;
     h)
         Usage
@@ -71,8 +109,24 @@ do
     esac
 done
 
+if [[ -n ${CNOPT} ]]; then
+    case ${CNOPT} in
+        [T])
+        CNSWITCH=1
+        ;;
+        [F])
+        CNSWITCH=0
+        ;;
+        *)
+        echo -e "${ERROR}: Invalid option, only ${hint}T${normal} or ${hint}F${normal} can be accepted by the ${hint}-s${normal} flag."
+        Usage
+        exit 1
+        ;;
+    esac
+fi
+
 # Setting a basic docker image
-if [[ ! -n ${RUNTIME} ]]; then
+if [[ -z ${RUNTIME} ]]; then
     echo -e "${WARNING}: no ${hint}-r${normal} (runtime) option was supplied, so automatically setting to \"${hint}normal${normal}\" runtime."
     RUNTIME="normal"
 fi
@@ -113,8 +167,10 @@ ENV NV_RUNTIME=TRUE
 ENV BASE="nvidia/cudagl:9.1-runtime-ubuntu16.04"
 ENV NVIDIA_VISIBLE_DEVICES ${NVIDIA_VISIBLE_DEVICES:-all}
 ENV NVIDIA_DRIVER_CAPABILITIES ${NVIDIA_DRIVER_CAPABILITIES:+$NVIDIA_DRIVER_CAPABILITIES,}graphics
+# CN_SP+
 RUN sed -i "s/archive.ubuntu.com/mirrors.tuna.tsinghua.edu.cn/g" /etc/apt/sources.list \
-    && echo "deb https://mirrors.aliyun.com/nvidia-cuda/ubuntu1604/x86_64/ ./" > /etc/apt/sources.list.d/cuda.list'  > build/base/Dockerfile
+    && echo "deb https://mirrors.aliyun.com/nvidia-cuda/ubuntu1604/x86_64/ ./" > /etc/apt/sources.list.d/cuda.list
+# CN_SP-'  > build/base/Dockerfile
     fi
 elif [[ ${RUNTIME} = "normal" ]]; then
     if [[ -z ${COMPOSE} ]]; then
@@ -127,7 +183,9 @@ FROM ubuntu:16.04
 # mesa runtime
 ENV NV_RUNTIME=FALSE
 ENV BASE="ubuntu:16.04"
+# CN_SP+
 RUN sed -i "s/archive.ubuntu.com/mirrors.tuna.tsinghua.edu.cn/g" /etc/apt/sources.list
+# CN_SP-
 RUN apt-get update -qq \
     && apt-get install -y -q --no-install-recommends \
            qt5-default \
@@ -143,12 +201,21 @@ else
     exit 1
 fi
 
+if [[ -z ${CNSWITCH} ]]; then
+    CNSWITCH=1
+fi
+
 if [[ -z ${COMPOSE} ]]; then
+    ## Copy default Dockerfile
+    cp build/Dockerfile_OG build/Dockerfile
+    ## CN_SP
+    cn_proxy build/base/Dockerfile ${CNSWITCH}
+    cn_proxy build/Dockerfile ${CNSWITCH}
     ## Build base image
     docker build -t radiolab_base:latest build/base
     echo -e "${PROCEED}: Base image build complete"
 
     # Build Docker image with proper runtime
     echo -e "${PROCEED}: Build \"${hint}radiolab${normal}\" image from base"
-    docker build -t radiolab:latest build --build-arg RUNTIME=$RUNTIME
+    docker build -t radiolab:latest build --build-arg RUNTIME=$RUNTIME --build-arg SYS_BUILD_DATE=UTC-$(date -u '+%Y-%m-%d')
 fi
